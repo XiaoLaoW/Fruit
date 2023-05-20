d5ca2d2c2a125e0bf6d089662a13b84397395442
XiaoLaoW <132039857+XiaoLaoW@users.noreply.github.com>
2023-05-20 23:02:00 +0800

    Add files via upload

--- src/app.config.ts
+++ src/app.config.ts
@@ -1,32 +1,32 @@
 export default defineAppConfig({
   pages: [
-    'pages/login/index',
-    'pages/index/index',
-    'pages/my/index',
-    'pages/order/index',
-    'pages/my/history/index',
-    'pages/my/orderList/index',
+    "pages/login/index",
+    "pages/index/index",
+    "pages/my/index",
+    "pages/order/index",
+    "pages/my/history/index",
+    "pages/my/orderList/index",
   ],
   window: {
-    backgroundTextStyle: 'light',
-    navigationBarBackgroundColor: '#2CC2C3',
-    navigationBarTitleText: '拼果团',
-    navigationBarTextStyle: 'black',
+    backgroundTextStyle: "light",
+    navigationBarBackgroundColor: "#2CC2C3",
+    navigationBarTitleText: "拼果团",
+    navigationBarTextStyle: "black",
   },
   tabBar: {
-    color: '#fff',
-    selectedColor: '#2a2d2d',
-    backgroundColor: '#98c9c8',
-    borderStyle: 'black',
+    color: "#fff",
+    selectedColor: "#2a2d2d",
+    backgroundColor: "#2CC2C3",
+    borderStyle: "black",
     list: [
       {
-        pagePath: 'pages/index/index',
-        text: '首页',
+        pagePath: "pages/index/index",
+        text: "首页",
       },
       {
-        pagePath: 'pages/my/index',
-        text: '我的',
+        pagePath: "pages/my/index",
+        text: "我的",
       },
     ],
   },
-})
+});
--- src/pages/my/my.jsx
+++ src/pages/my/my.jsx
@@ -1,52 +1,86 @@
-import { View } from '@tarojs/components'
-import Taro from '@tarojs/taro'
-import { useState } from 'react'
-import { dateToString } from '../../utils/utils'
+import { View } from "@tarojs/components";
+import Taro from "@tarojs/taro";
+import { useState } from "react";
+import { dateToString } from "../../utils/utils";
 export default function History() {
-  const stamp1 = dateToString(new Date(new Date().setHours(0, 0, 0, 0))) //获取当天零点的时间
-  console.log(stamp1)
+  const stamp1 = dateToString(new Date(new Date().setHours(0, 0, 0, 0))); //获取当天零点的时间
+  console.log(stamp1);
+
   //查看本端口历史单据
   function history() {
-    Taro.navigateTo({ url: './history/index' })
+    Taro.navigateTo({ url: "./history/index" });
   }
   //查看所有人的商品汇总
   async function orderList() {
-    const c1 = new Taro.cloud.Cloud({
-      resourceEnv: 'test-taro1-4gdydbsi405487f2',
-    })
-    const db = Taro.cloud.database({})
-    const _ = db.command
-    const result = await db
-      .collection('orderList')
+    //判断是否有权限
+    Taro.showModal({
+      editable: true,
+      placeholderText: "请输入密码",
+      success: async function (res) {
+        if (res.confirm && res.content == 123456) {
+          console.log(res);
+          const c1 = new Taro.cloud.Cloud({
+            resourceEnv: "test-taro1-4gdydbsi405487f2",
+          });
+          const db = Taro.cloud.database({});
+          const _ = db.command;
+          const result = await db
+            .collection("orderList")
 
-      .where({
-        time: _.gt(stamp1),
-      })
-      .get()
-    console.log(result)
+            .where({
+              time: _.gt(stamp1),
+            })
+            .get();
+          console.log(result);
 
-    const ResultList = []
-    ResultList.value = result
-    Taro.preload({ ResultList: result })
-    Taro.navigateTo({ url: './orderList/index' })
+          const ResultList = [];
+          ResultList.value = result;
+          Taro.preload({ ResultList: result });
+          Taro.navigateTo({ url: "./orderList/index" });
+        } else if (res.cancel) {
+          console.log("用户点击取消");
+        } else {
+          Taro.showToast({
+            title: "密码错误",
+            icon: "error",
+          });
+        }
+      },
+    });
   }
   //清空数据库
   async function clearList() {
-    const c1 = new Taro.cloud.Cloud({
-      resourceEnv: 'test-taro1-4gdydbsi405487f2',
-    })
-    const db = Taro.cloud.database({})
-    await db
-      .collection('orderList')
-      .where({
-        all: null,
-      })
-      .remove()
-    Taro.showToast({
-      title: '删除成功',
-      icon: 'success',
-      duration: 1000,
-    })
+    //判断权限
+    Taro.showModal({
+      editable: true,
+      placeholderText: "请输入密码",
+      success: async (res) => {
+        if (res.confirm && res.content == 123456) {
+          const c1 = new Taro.cloud.Cloud({
+            resourceEnv: "test-taro1-4gdydbsi405487f2",
+          });
+          const db = Taro.cloud.database({});
+          await db
+            .collection("orderList")
+            .where({
+              all: null,
+            })
+            .remove();
+          Taro.showToast({
+            title: "删除成功",
+            icon: "success",
+            duration: 1000,
+          });
+        } else if (res.cancel) {
+          console.log("用户取消");
+        } else {
+          Taro.showToast({
+            title: "密码错误",
+            icon: "error",
+          });
+        }
+      },
+    });
   }
   return (
     <>
@@ -60,5 +94,5 @@ export default function History() {
         <text onClick={() => clearList()}>删库</text>
       </View>
     </>
-  )
+  );
 }
