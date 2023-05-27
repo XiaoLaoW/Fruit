420b3b8ad755c22460a540f5e51dd01cacebafed
XiaoLaoW <132039857+XiaoLaoW@users.noreply.github.com>
2023-05-27 16:25:24 +0800

    Add files via upload

--- src/pages/my/my.jsx
+++ src/pages/my/my.jsx
@@ -1,90 +1,93 @@
-import { View } from "@tarojs/components";
-import Taro from "@tarojs/taro";
-import { useState } from "react";
-import { dateToString } from "../../utils/utils";
+import { View } from '@tarojs/components'
+import Taro from '@tarojs/taro'
+import { useState } from 'react'
+// import { dateToString } from '../../utils/utils'
 export default function History() {
-  const stamp1 = dateToString(new Date(new Date().setHours(0, 0, 0, 0))); //获取当天零点的时间
-  console.log(stamp1);
-
+  const [res, setRes] = useState()
+  Taro.cloud
+    .callFunction({
+      // 要调用的云函数名称
+      name: 'orderList',
+      // 传递给云函数的event参数
+    })
+    .then((res) => {
+      setRes(res.result.data)
+    })
+    .catch((err) => {
+      console.log(err)
+    })
+  // const stamp1 = dateToString(new Date(new Date().setHours(0, 0, 0, 0))) //获取当天零点的时间
   //查看本端口历史单据
   function history() {
-    Taro.navigateTo({ url: "./history/index" });
+    Taro.navigateTo({ url: './history/index' })
   }
   //查看所有人的商品汇总
   async function orderList() {
     const c1 = new Taro.cloud.Cloud({
-      resourceEnv: "test-taro1-4gdydbsi405487f2",
-    });
-    const db = Taro.cloud.database({});
-    const _ = db.command;
-    const result = await db
-      .collection("orderList")
+      resourceEnv: 'test-taro1-4gdydbsi405487f2',
+    })
 
-      .where({
-        time: _.gt(stamp1),
-      })
-      .get();
-
-    const ResultList = [];
-    ResultList.value = result;
-    Taro.preload({ ResultList: result });
-    Taro.navigateTo({ url: "./orderList/index" });
+    const ResultList = []
+    ResultList.value = res
+    console.log(res)
+    Taro.preload({ ResultList: res })
+    Taro.navigateTo({ url: './orderList/index' })
   }
   //清空数据库
   async function clearList() {
     //判断权限
     Taro.showModal({
       editable: true,
-      placeholderText: "请输入密码",
+      placeholderText: '请输入密码',
       success: async (res) => {
         if (res.confirm && res.content == 1) {
           const c1 = new Taro.cloud.Cloud({
-            resourceEnv: "test-taro1-4gdydbsi405487f2",
-          });
-          const db = Taro.cloud.database({});
+            resourceEnv: 'test-taro1-4gdydbsi405487f2',
+          })
+          const db = Taro.cloud.database({})
           await db
-            .collection("orderList")
+            .collection('orderList')
             .where({
               all: null,
             })
-            .remove();
+            .remove()
           Taro.showToast({
-            title: "删除成功",
-            icon: "success",
+            title: '删除成功',
+            icon: 'success',
             duration: 1000,
-          });
+          })
         } else if (res.cancel) {
-          console.log("用户取消");
+          console.log('用户取消')
         } else {
           Taro.showToast({
-            title: "密码错误",
-            icon: "error",
-          });
+            title: '密码错误',
+            icon: 'error',
+          })
         }
       },
-    });
+    })
   }
   //更新商品列表
   function upDate() {
     //判断权限
     Taro.showModal({
       editable: true,
-      placeholderText: "请输入密码",
+      placeholderText: '请输入密码',
       success: async (res) => {
         if (res.confirm && res.content == 1) {
           Taro.navigateTo({
-            url: "./upDate/index",
-          });
+            url: './upDate/index',
+          })
         } else if (res.cancel) {
-          console.log("用户取消");
+          console.log('用户取消')
         } else {
           Taro.showToast({
-            title: "密码错误",
-            icon: "error",
-          });
+            title: '密码错误',
+            icon: 'error',
+          })
         }
       },
-    });
+    })
   }
   return (
     <>
@@ -102,5 +105,5 @@ export default function History() {
         <text onClick={() => clearList()}>删库</text>
       </View>
     </>
-  );
+  )
 }
--- src/pages/my/orderList/list.jsx
+++ src/pages/my/orderList/list.jsx
@@ -1,65 +1,63 @@
-import { View, Image, Button } from "@tarojs/components";
-import Taro from "@tarojs/taro";
-import { useState } from "react";
-import "./index.scss";
-import { getArrayIndex } from "../../../utils/utils";
+import { View, Image, Button } from '@tarojs/components'
+import Taro from '@tarojs/taro'
+import { useState } from 'react'
+import './index.scss'
+import { getArrayIndex } from '../../../utils/utils'
 export default function List() {
-  const List = Taro.getCurrentInstance().preloadData;
-  const result = List.ResultList.data;
-  console.log(result);
+  const result = Taro.getCurrentInstance().preloadData.ResultList
   //计算总金额
-  const totalPrice = [];
+  const totalPrice = []
 
   //遍历商品
-  const firstArray = [];
+  const firstArray = []
   for (var i = 0; i < result.length; i++) {
-    var Price = result[i].totalPrice;
-    var clientName = result[i].clientName;
-    var price = result[i].totalPrice;
-    totalPrice.push(Price);
-    var secondArray = result[i].array;
+    var Price = result[i].totalPrice
+    var clientName = result[i].clientName
+    var price = result[i].totalPrice
+    totalPrice.push(Price)
+    var secondArray = result[i].array
     for (var j = 0; j < secondArray.length; j++) {
       if (j === 0) {
         firstArray.push({
           name: clientName,
           count: 0,
-          title: "购买人",
+          title: '购买人',
           price: price,
-        });
+        })
       } else {
       }
-      firstArray.push(secondArray[j]);
+      firstArray.push(secondArray[j])
     }
   }
-  console.log(firstArray, totalPrice);
+  console.log(firstArray, totalPrice)
   var countSum = firstArray.reduce(function (prev, cur) {
-    return cur.count + prev;
-  }, 0);
+    return cur.count + prev
+  }, 0)
   var priceSum = totalPrice.reduce(function (prev, cur) {
-    return cur + prev;
-  }, 0);
+    return cur + prev
+  }, 0)
 
-  console.log(countSum, priceSum);
+  console.log(countSum, priceSum)
   //数组去重,买了哪些商品
   let newArr = firstArray.reduce((pre, cur) => {
     if (!pre.includes(cur.title)) {
-      return pre.concat(cur.title);
+      return pre.concat(cur.title)
     } else {
-      return pre;
+      return pre
     }
-  }, []);
-  console.log(newArr);
-  const productsCount = [];
+  }, [])
+  console.log(newArr)
+  const productsCount = []
   for (var i = 0; i < newArr.length; i++) {
-    var title = newArr[i];
-    var newFirst = firstArray.filter((item) => item.title == title);
-    console.log(newFirst);
+    var title = newArr[i]
+    var newFirst = firstArray.filter((item) => item.title == title)
+    console.log(newFirst)
     var count = newFirst.reduce(function (prev, cur) {
-      return cur.count + prev;
-    }, 0);
-    productsCount.push(count);
+      return cur.count + prev
+    }, 0)
+    productsCount.push(count)
 
-    console.log(productsCount);
+    console.log(productsCount)
   }
 
   return (
@@ -81,24 +79,22 @@ export default function List() {
           {newArr.map((item) => {
             return (
               <view
-                style={item == "购买人" ? "display:none" : "display:flex"}
-                className="totalTitle"
-              >
+                style={item == '购买人' ? 'display:none' : 'display:flex'}
+                className="totalTitle">
                 品名:{item}
               </view>
-            );
+            )
           })}
         </View>
         <View>
           {productsCount.map((item) => {
             return (
               <view
-                style={item === 0 ? "display:none" : "display:flex"}
-                className="totalCount"
-              >
+                style={item === 0 ? 'display:none' : 'display:flex'}
+                className="totalCount">
                 总件数:{item}件
               </view>
-            );
+            )
           })}
         </View>
       </View>
@@ -108,27 +104,25 @@ export default function List() {
           return (
             <>
               <view
-                style={item.count != 0 ? "display:flex" : "display:none"}
+                style={item.count != 0 ? 'display:flex' : 'display:none'}
                 className="list-content"
-                key={firstArray.index}
-              >
+                key={firstArray.index}>
                 <view className="name">{item.title}</view>
                 <view className="count">数量:{item.count}</view>
                 <view className="price">单价:{item.price}</view>
               </view>
               <view
-                style={item.count === 0 ? "display:flex" : "display:none"}
-                className="clientName"
-              >
+                style={item.count === 0 ? 'display:flex' : 'display:none'}
+                className="clientName">
                 <view className="name">购买人:{item.name}</view>
                 <view className="price">
                   本次应付:{item.price.toFixed(2)}元
                 </view>
               </view>
             </>
-          );
+          )
         })}
       </View>
     </>
-  );
+  )
 }
